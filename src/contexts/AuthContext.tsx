1b4d40e02afb6ae6cc1c57c54fcb81b2a6a42944
Endra Sim <endra.sim@gmail.com>
2025-11-12 20:33:12 +1300

    Update API endpoints for login and logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   const login = async (username: string, password: string) => {
-    const response = await fetch('http://3.27.235.209:8000/api/login/', {
+    const response = await fetch('http://4.196.72.83:8000/api/login/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const logout = async () => {
     if (user) {
       try {
-        await fetch('http://3.27.235.209:8000/api/logout/', {
+        await fetch('http://4.196.72.83:8000/api/logout/', {
           method: 'POST',
           headers: {
             'Authorization': `token ${user.token}`,
